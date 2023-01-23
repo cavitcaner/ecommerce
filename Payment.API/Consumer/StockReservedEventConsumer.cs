73e746520686f37022ee5471af7423701d523fcb
cavitcaner <50204510+cavitcaner@users.noreply.github.com>
2023-01-23 14:23:17 +0300

    bugfix

--- Payment.API/Consumer/StockReservedEventConsumer.cs
+++ Payment.API/Consumer/StockReservedEventConsumer.cs
@@ -1,4 +1,5 @@
-﻿using Common.Payment;
+﻿using Common;
+using Common.Payment;
 using Common.Stock;
 using MassTransit;
 
@@ -33,12 +34,24 @@ namespace Payment.API.Consumer
             }
 
             Console.WriteLine("Ödeme başarılı bir şekilde alındı!");
-            await _publishEndpoint.Publish(new PaymentSuccessEvent
+            var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:" + QueueConst.InvoicePaymentSuccessEventName));
+
+            await sendEndpoint.Send(new PaymentSuccessEvent
             {
                 CustomerEmail = context.Message.CustomerEmail,
                 OrderId = context.Message.OrderId,
                 OrderItems = context.Message.OrderItems
             });
+            
+            var sendEndpoint1 = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:" + QueueConst.PaymentSuccessEventQueueName));
+
+            await sendEndpoint1.Send(new PaymentSuccessEvent
+            {
+                CustomerEmail = context.Message.CustomerEmail,
+                OrderId = context.Message.OrderId,
+                OrderItems = context.Message.OrderItems
+            });
+
         }
     }
 }
